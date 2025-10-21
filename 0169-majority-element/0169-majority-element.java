904b2fd796a8c8157f7f738b47780dc1846ee231
SHASHANK SHEKHAR <130763452+Shashankshekhar05@users.noreply.github.com>
2025-10-21 15:07:26 +0530

    Time: 2 ms (74.8%), Space: 53.3 MB (31.16%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+     int majority_element=-1;
+     int count =0;
+     for(int i=0;i<nums.length;i++){
+        if(count==0){
+        majority_element=nums[i];
+          count=1;
+        }
+
+          else if(nums[i]==majority_element){
+            count++;
+          }
+          else{
+          count--;
+          }
+     }
+   int freq=0;
+   for(int i=0;i<nums.length;i++){
+    if(majority_element==nums[i]){
+        freq++;
+    }
+   }
+   if(freq>nums.length/2){
+    return majority_element;
+   }
+        
+      return -1;
+    }
+    
+}
