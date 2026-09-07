5c18061d15288e217cb65366a8edf49793b069a6
SHASHANK SHEKHAR <130763452+Shashankshekhar05@users.noreply.github.com>
2026-09-07 18:38:16 +0530

    Time: 3 ms (54.55%), Space: 13.2 MB (53.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        seen_map={}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return i ,j
+            required_num=target-nums[i]
+            if required_num in seen_map:
+                return[seen_map[required_num],i]
+
+            seen_map[nums[i]]=  i     
+        
 
                 
         """
